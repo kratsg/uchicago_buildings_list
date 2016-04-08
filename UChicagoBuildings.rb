1ce1c494f979f4da92c0c596afdf682260175383
Giordon Stark <kratsg@gmail.com>
2016-04-07 19:57:38 -0500

    update to latest

--- UChicagoBuildings.rb
+++ UChicagoBuildings.rb
@@ -4,8 +4,8 @@ uri = URI 'http://registrar.uchicago.edu/page/building-abbreviations-addresses'
 response = Net::HTTP.get(uri).gsub!(/(\n|\t)/,"")
 response_doc = Nokogiri::HTML(response)
 data = response_doc.css('.maincontent table tbody tr').map do |row|
-  abbr = row.css('td:nth-child(1) h2 strong').text
-  data = row.css('td:nth-child(2) h2').text
+  abbr = row.css('td:nth-child(1) h2 a').text
+  data = row.css('td:nth-child(2) h3').text
   name, addr = data.split(",", 2)
   next if addr.nil?
   abbr.strip!
@@ -15,3 +15,4 @@ data = response_doc.css('.maincontent table tbody tr').map do |row|
 end
 
 data.compact
+p data
